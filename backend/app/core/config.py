87741faddbf7342a0665782586c77c23c3e5dfb9
Muneeb Khawaja <36654508+mtkhawaja@users.noreply.github.com>
2021-04-01 02:50:07 -0400

    Configured default redirect URL as an env var.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,3 +7,6 @@ HOST_NAME = config("HOST_NAME", default="0.0.0.0")
 HOST_PORT = config("HOST_PORT", default=8888)
 DEBUG = config("DEBUG", default=True, cast=bool)
 SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
+NOT_FOUND_REDIRECT = config(
+    "NOT_FOUND_REDIRECT", default="http://localhost:8000/pages/404"
+)

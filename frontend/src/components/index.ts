9566bed0dec43c014d6bc66a94bcd5bee4ca8031
Muneeb Khawaja <36654508+mtkhawaja@users.noreply.github.com>
2021-04-01 03:00:09 -0400

    Added component exports.

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './Navigation';
+export * from './Redirector';
+export * from './ShortlinkModal';
+export * from './URLForm';
+export * from './CopyToast';
--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -4,3 +4,4 @@ export * from './Login';
 export * from './SignUp';
 export * from './Protected';
 export * from './PrivateRoute';
+export * from './NotFound404';
